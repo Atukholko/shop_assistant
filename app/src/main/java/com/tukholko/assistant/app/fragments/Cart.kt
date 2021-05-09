21658312425bebf6e95bc79855b75994f3c5e91c
Alexey Tukholko <a.tukholko@gmail.com>
2021-05-09 18:54:18 +0300

    Added empty cart view

--- app/src/main/java/com/tukholko/assistant/app/fragments/Cart.kt
+++ app/src/main/java/com/tukholko/assistant/app/fragments/Cart.kt
@@ -1,6 +1,8 @@
 package com.tukholko.assistant.app.fragments
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -12,15 +14,15 @@ import androidx.recyclerview.widget.RecyclerView
 import com.tukholko.assistant.R
 import com.tukholko.assistant.app.RecyclerAdapter
 
+
 class Cart : Fragment() {
     var text: TextView? = null
     var button: Button? = null
     var counter: Int = 0
 
-    var recyclerView : RecyclerView? = null;
-    var adapter : RecyclerAdapter? = null;
-
-    var totalPriceView: TextView? = null;
+    var recyclerView : RecyclerView? = null
+    var emptyCartView : TextView? = null
+    var adapter : RecyclerAdapter? = null
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_left, container, false)
@@ -28,17 +30,45 @@ class Cart : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        emptyCartView = view.findViewById(R.id.cart_empty)
+        var totalPriceView: TextView = view.findViewById(R.id.total_price)
+        totalPriceView.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable) {}
+            override fun beforeTextChanged(s: CharSequence, start: Int,
+                                           count: Int, after: Int) {
+            }
 
+            override fun onTextChanged(s: CharSequence, start: Int,
+                                       before: Int, count: Int) {
+                if (s == "0") {
+                    recyclerView?.visibility = View.GONE
+                    emptyCartView?.visibility = View.VISIBLE
+                } else {
+                    recyclerView?.visibility = View.VISIBLE
+                    emptyCartView?.visibility = View.GONE
+                }
+
+            }
+        })
         //Recycler
         recyclerView = view.findViewById(R.id.recycler_cart)
         recyclerView?.layoutManager = LinearLayoutManager(this.context)
         adapter = RecyclerAdapter(this.context)
         recyclerView?.adapter = adapter
-        adapter!!.setTotalPriceView(view.findViewById<TextView>(R.id.total_price))
+
+        adapter!!.setTotalPriceView(totalPriceView)
+
     }
 
     override fun onStart() {
         super.onStart()
+        if (adapter?.itemCount == 0) {
+            recyclerView?.visibility = View.GONE
+            emptyCartView?.visibility = View.VISIBLE
+        } else {
+            recyclerView?.visibility = View.VISIBLE
+            emptyCartView?.visibility = View.GONE
+        }
     }
 
     companion object {
