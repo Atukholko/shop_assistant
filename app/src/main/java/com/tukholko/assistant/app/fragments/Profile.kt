28e3733e06de349d045cf8edf33d5e2e5e45f339
Alexey Tukholko <a.tukholko@gmail.com>
2021-05-09 13:23:13 +0300

    Implemented getUser method for profile fragment

--- app/src/main/java/com/tukholko/assistant/app/fragments/Profile.kt
+++ app/src/main/java/com/tukholko/assistant/app/fragments/Profile.kt
@@ -1,6 +1,7 @@
 package com.tukholko.assistant.app.fragments
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,9 +11,14 @@ import androidx.fragment.app.Fragment
 import com.google.firebase.auth.FirebaseAuth
 import com.tukholko.assistant.R
 import com.tukholko.assistant.app.AppActivity
+import com.tukholko.assistant.app.service.NetworkService
 import com.tukholko.assistant.model.User
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class Profile : Fragment() {
+    val TAG = "PROFILE"
     lateinit var email: TextView
     lateinit var firstName: TextView
     lateinit var secondName: TextView
@@ -23,17 +29,40 @@ class Profile : Fragment() {
         return inflater.inflate(R.layout.fragment_profile, container, false)
     }
 
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        getUser(auth.currentUser?.email.toString())
         email = view.findViewById(R.id.profileEmail)
-        email.text = auth.currentUser?.email ?: "Error"
+        firstName = view.findViewById(R.id.profileName)
+        secondName = view.findViewById(R.id.profileSecondName)
+
         view.findViewById<Button>(R.id.signOut).setOnClickListener {
             (activity as AppActivity?)!!.signOut()
         }
         super.onViewCreated(view, savedInstanceState)
     }
 
+    private fun fillProfile(user: User) {
+        email.text = user.email
+        firstName.text = user.firstName
+        secondName.text = user.secondName
+    }
+
     private fun getUser(email: String) {
-        //TODO make reading from db
+        NetworkService.getInstance()
+                .userAPI
+                .getUserByEmail(email).enqueue(object : Callback<User> {
+                    override fun onResponse(call: Call<User>, response: Response<User>) {
+                        Log.i(TAG, "user is wrote into db")
+                        user = response.body() as User
+                        fillProfile(user)
+                    }
+
+                    override fun onFailure(call: Call<User>, t: Throwable) {
+                        Log.i(TAG, "user is not wrote into db")
+                        user = User(email, "", "")
+                    }
+                })
     }
 
     companion object {
